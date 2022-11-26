8d9549cd7f50b4f8e54139d4393559ea933007bd
unknown <Aleksandr2697@yandex.ru>
2022-11-27 00:36:40 +0300

    sd

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,22 +2,17 @@ import jwt from 'jsonwebtoken';
 import { UnauthorizedError } from '../errors/UnauthorizedError.js';
 
 export const auth = (req, res, next) => {
-  const { authorization } = req.headers;
-
+  const { authorization = '' } = req.headers;
   if (!authorization) {
-    throw new UnauthorizedError('Необходима авторизация');
-  }
-  const token = authorization.replace('Bearer ', '');
-
-  let payload;
-
-  try {
-    payload = jwt.verify(token, 'secret');
-  } catch (err) {
     next(new UnauthorizedError('Необходима авторизация'));
+  } else {
+    const token = authorization.replace(/^Bearer*\s*/i, '');
+    try {
+      const decoded = jwt.verify(token, 'secret');
+      req.user = { _id: decoded._id };
+      next();
+    } catch (err) {
+      next(new UnauthorizedError('Необходима авторизация'));
+    }
   }
-
-  req.user = payload;
-
-  return next();
 };
