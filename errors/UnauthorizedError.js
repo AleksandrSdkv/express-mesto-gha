d7411041df84207c05c20f9d4aeb11d4450a438d
unknown <Aleksandr2697@yandex.ru>
2022-11-26 23:06:28 +0300

    401

--- errors/UnauthorizedError.js
+++ errors/UnauthorizedError.js
@@ -1,6 +1,9 @@
+import { constants } from 'http2';
+
 export class UnauthorizedError extends Error {
   constructor(message) {
     super(message);
-    this.statusCode = 401;
+    this.name = 'UnauthorizedError';
+    this.statusCode = constants.HTTP_STATUS_UNAUTHORIZED;
   }
 }
