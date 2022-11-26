3ecfca1e8954a7ad272ebdc32395a908cba94daa
unknown <Aleksandr2697@yandex.ru>
2022-11-27 00:07:32 +0300

    fx3

--- errors/BadRequestError.js
+++ errors/BadRequestError.js
@@ -1,8 +1,9 @@
 import { constants } from 'http2';
-import { HTTPError } from './ForbiddenError.js';
 
-export class BadRequestError extends HTTPError {
+export class BadRequestError extends Error {
   constructor(message) {
-    super(message, constants.HTTP_STATUS_BAD_REQUEST);
+    super(message);
+    this.name = 'BadRequestError';
+    this.statusCode = constants.HTTP_STATUS_BAD_REQUEST;
   }
 }
