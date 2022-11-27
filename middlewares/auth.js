30add75865bcadba3f6341fd0bc56d02d9315667
unknown <Aleksandr2697@yandex.ru>
2022-11-27 20:09:58 +0300

    qwe

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,26 +1,18 @@
 import jwt from 'jsonwebtoken';
+import { UnauthorizedError } from '../errors/UnauthorizedError.js';
 
-// eslint-disable-next-line consistent-return
-export const auth = (req, res) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
-  }
-
-  const token = authorization.replace(/^Bearer*\s*/i, '');
-  let payload;
-
-  try {
-    // попытаемся верифицировать токен
-    // eslint-disable-next-line no-unused-vars
-    payload = jwt.verify(token, 'some-secret-key');
-  } catch (err) {
-    // отправим ошибку, если не получилось
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+export const auth = (req, res, next) => {
+  const { authorization = '' } = req.headers;
+  if (!authorization) {
+    next(new UnauthorizedError('Требуется аутентификация'));
+  } else {
+    const token = authorization.replace(/^Bearer*\s*/i, '');
+    try {
+      const decoded = jwt.verify(token, 'some-secret-key');
+      req.user = { _id: decoded._id };
+      next();
+    } catch (err) {
+      next(new UnauthorizedError('Требуется аутентификация'));
+    }
   }
 };
